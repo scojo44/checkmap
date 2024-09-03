44602938ec94c3b3f4bce63153a419b1c7dc85ee
scojo44 <Scott@scojo.org>
2024-09-03 16:31:16 -0700

    Change polygon color demo

--- checkmap-fe/src/App.jsx
+++ checkmap-fe/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {MapContainer, TileLayer, Polygon} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
 import './App.css'
 
 function App() {
+  const [purple, setPurple] = useState(true);
 
   return (
-    <>
-    </>
+    <MapContainer center={[48.75, -116.25]} zoom={11}>
+      <TileLayer
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Polygon positions={[[48.75, -116.25], [48.6, -116], [48.75, -116]]}
+        pathOptions={{color: purple? 'mediumpurple':'cornflowerblue'}}
+        eventHandlers={{
+          click: (e) => {setPurple(c => !c);}
+        }}
+      />
+    </MapContainer>
   );
 }
 
